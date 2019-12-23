adeebdb920bd0ac5e038c83d9efc68c33158e2cd
Agambale <antonioga7>
2019-12-23 11:51:06 +0100

    Aggiornamento CarrelloItem

--- Full-Beer/src/beans/carrelloItem.java
+++ Full-Beer/src/beans/carrelloItem.java
@@ -1,5 +1,35 @@
 package beans;
-
+/**
+ * Questa classe modella gli elementi del carrello
+ */
 public class carrelloItem {
 
+	/**costruttore carrelloItem*/
+	public carrelloItem(){
+		
+		int qt = 1;
+	}
+/**
+ * permette do ottenere la variabile d'istanza che moedella il prodotto*/
+	public prodottoB getProdotto() {
+		return prodotto;
+	}
+	
+/**
+ * permette di settare la variabile d'istanza che modella il prodotto*/	
+	public void setProdotto(prodottoB prodotto) {
+		this.prodotto = prodotto;
+	}
+
+/*permette di ottenere la variabile quantità*/
+	public int getQt() {
+		return qt;
+	}
+/*permette di settare la variabile quantità*/
+	public void setQt(int qt) {
+		this.qt = qt;
+	}
+
+private int qt;
+private prodottoB prodotto; 
 }
