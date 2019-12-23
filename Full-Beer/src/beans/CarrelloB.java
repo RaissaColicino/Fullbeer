2007dd9119b54592fa42dfcb7c0d59defa0827d8
Agambale <antonioga7>
2019-12-23 12:07:59 +0100

    Inizio Creazione CarrelloB

--- Full-Beer/src/beans/CarrelloB.java
+++ Full-Beer/src/beans/CarrelloB.java
@@ -1,5 +1,17 @@
 package beans;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * classe per modellare il carrello singolo di un utente*/
 public class CarrelloB {
 
+	/**
+	 * Costruttore*/
+		public CarrelloB(){
+			
+			Carrello=new LinkedHashSet<carrelloItem>();
+		}
+ private Set<carrelloItem> Carrello;
 }
