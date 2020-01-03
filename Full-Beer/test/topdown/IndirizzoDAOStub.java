45970edab0dc7149cb23baf3c0aa93bcf95ba7f5
RaissaColicino <56045285+RaissaColicino@users.noreply.github.com>
2020-01-03 17:16:13 +0100

    Implementazione Stub

---
+++ Full-Beer/test/topdown/IndirizzoDAOStub.java
@@ -0,0 +1,38 @@
+package topdown;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.logging.Logger;
+
+import beans.IndirizzoB;
+import beans.UtenteB;
+
+
+public class IndirizzoDAOStub {
+	static Logger log=Logger.getLogger("IndirizzoStubDebugger");
+	
+
+	public IndirizzoDAOStub() {
+		
+	}
+	
+
+	public Set<IndirizzoB> doRetrieveByUtente(UtenteB user){
+		LinkedHashSet<IndirizzoB> indirizzi=new LinkedHashSet<IndirizzoB>();
+
+		IndirizzoB indirizzoA=new IndirizzoB();
+		indirizzoA.setUsername(user.getUsername());
+		indirizzoA.setCap("83040");
+		indirizzoA.setCittà("Montemarano");
+		indirizzoA.setVia("C/da Macchia del monte,14");
+		indirizzi.add(indirizzoA);
+		
+		IndirizzoB indirizzoB=new IndirizzoB();
+		indirizzoB.setUsername(user.getUsername());
+		indirizzoB.setCap("83048");
+		indirizzoB.setCittà("Montella");
+		indirizzoB.setVia("Via San Francesco");
+		indirizzi.add(indirizzoB);
+		
+		return indirizzi;
+	}
+}
